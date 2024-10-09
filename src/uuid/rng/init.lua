aa2510b55957b6357fe94dd7b90aff97b372f760
rfl890 <87506407+rfl890@users.noreply.github.com>
2024-10-09 03:20:31 -0400

    Add FFI generator for Windows (#18)

--- src/uuid/rng/init.lua
+++ src/uuid/rng/init.lua
@@ -81,6 +81,60 @@ end
 
 
 do
+  local win_ffi_rng
+
+  ----------------------------------------------------------------------------
+  -- Returns an rng that implements Windows' RtlGenRandom function which is
+  -- a good source of randomness on all modern versions of Windows.
+  -- Requires LuaJIT or [`cffi-lua`](https://github.com/q66/cffi-lua).
+  -- @treturn function A function that returns `n` random bytes, signature: `byte_string, err = func(n)`
+  -- @usage
+  -- local uuid = require "uuid"
+  -- uuid.set_rng(uuid.rng.win_ffi())
+  function rng.win_ffi()
+    if package.config:sub(1, 1) ~= "\\" then
+      return nil, "win-ffi is only available on Windows"
+    end
+
+    -- return cahced rng function if we already created it
+    if win_ffi_rng then
+      return win_ffi_rng
+    end
+
+    local ok, ffi = pcall(require, "ffi")
+    if not ok then
+        ok, ffi = pcall(require, "cffi")
+    end
+    if not ok then
+      return nil, "ffi not available"
+    end
+
+    local ffi_defs = [[
+      unsigned char SystemFunction036(void *RandomBuffer, unsigned long RandomBufferLength);
+    ]]
+    ffi.cdef(ffi_defs)
+
+    local ok, advapi32 = pcall(ffi.load, "advapi32.dll")
+    if not ok then
+      return nil, "failed to load advapi32.dll"
+    end
+
+    win_ffi_rng = function(n)
+      local buffer = ffi.new("unsigned char[?]", n)
+      local ok = advapi32.SystemFunction036(buffer, n)
+      if ok == 0 then
+        return nil, "call to RtlGenRandom failed"
+      end
+      return ffi.string(buffer, n)
+    end
+
+    return win_ffi_rng
+  end
+end
+
+
+
+do
   local rnd = function(n)
     local file, err = io.open("/dev/urandom", "rb")
     if not file then
@@ -186,6 +240,7 @@ if lua_version >= 5.4 then
 end
 
 
+
 do
   -- create a table, on modern CPUs the ASLR will make this unique.
   -- we keep the table around, to prevent it being GC'ed and the address being reused
@@ -227,6 +282,13 @@ do
       local ur = rng.urandom()
       if ur then
         return ur(40)  -- this is crypto level, so good enough
+      end
+    end
+
+    do -- try RtlGenRandom() via FFI on windows
+      local wf = rng.win_ffi()
+      if wf then
+        return wf(40)  -- this is crypto level, so good enough
       end
     end
 
@@ -296,6 +358,7 @@ do
     return rng.math_randomseed(seed1, seed2)
   end
 end
+
 
 
 return rng
